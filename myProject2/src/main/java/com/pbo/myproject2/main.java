884e76ca883dbb1d0520415983b237eacd9f6e5a
Rizky Akbar Wahyu Putranto <89757057+RizkyAkb@users.noreply.github.com>
2022-08-28 14:40:34 +0700

    first commit

---
+++ myProject2/src/main/java/com/pbo/myproject2/main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.pbo.myproject2;
+
+/**
+ *
+ * @author CoolFace
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("RRRRRRR      III      ZZZZZZZZZZ");   
+        System.out.println("R      R      I               Z");  
+        System.out.println("R      R      I             Z");  
+        System.out.println("RRRRRRR       I           Z");   
+        System.out.println("R R           I          Z");        
+        System.out.println("R   R         I        Z");      
+        System.out.println("R      R     III      ZZZZZZZZZZ");   
+    }
+}
